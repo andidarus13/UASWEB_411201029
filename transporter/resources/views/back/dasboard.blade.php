894d21267c7fda12c775da5f7c6920dd5ced871f
andidarus13 <134087223+andidarus13@users.noreply.github.com>
2023-07-15 11:31:28 +0700

    Update dasboard.blade.php

--- transporter/resources/views/back/dasboard.blade.php
+++ transporter/resources/views/back/dasboard.blade.php
@@ -195,6 +195,31 @@
 		</div>
 	</div> -->
 </div>
+    <div class="col-md-6">
+							<div class="card full-height">
+								<div class="card-body">
+									<div class="card-title">Statistic Pemasukan dan Pengeluaran</div>
+									<div class="row py-3">
+										<div class="col-md-4 d-flex flex-column justify-content-around">
+											<div>
+												<h6 class="fw-bold text-uppercase text-success op-8">Total Pemasukan</h6>
+												<h3 class="fw-bold">Rp. 150.000.000</h3>
+											</div>
+											<div>
+												<h6 class="fw-bold text-uppercase text-danger op-8">Total Pengeluaran</h6>
+												<h3 class="fw-bold">Rp. 30.000.000</h3>
+											</div>
+										</div>
+										<div class="col-md-8">
+											<div id="chart-container">
+												<canvas id="totalIncomeChart"></canvas>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
 @endsection
 
 
